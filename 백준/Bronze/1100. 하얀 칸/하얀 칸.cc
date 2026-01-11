986c0cf7cdd3953bb89529f76c361748061b8415
kkeagle3 <141285527+kkeagle3@users.noreply.github.com>
2026-01-11 18:35:50 +0900

    [Bronze II] Title: 하얀 칸, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1100. 하얀 칸/하얀 칸.cc
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.cc
@@ -7,17 +7,9 @@ int main()
     for(int i=0;i<8;i++){
         string line;
         cin >> line;
-        if(i%2==0){ 
-            for(int j=0;j<8;j+=2){
-                if(line[j]=='F')
-                    cnt++;
-            }
-        }
-        else{
-            for(int j=1;j<8;j+=2){
-                if(line[j]=='F')
-                    cnt++;
-            }
+        for(int j=i%2;j<8;j+=2){
+            if(line[j]=='F')
+                cnt++;
         }
     }
     cout << cnt;
