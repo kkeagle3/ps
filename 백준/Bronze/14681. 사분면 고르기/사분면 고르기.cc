e3a8ade025e60cda76bd215b08db4b204042ecee
kkeagle3 <141285527+kkeagle3@users.noreply.github.com>
2026-01-15 22:02:28 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    if(a>0 && b>0)
+        cout << 1;
+    else if(a>0 && b<0)
+        cout << 4;
+    else if(a<0 && b>0)
+        cout << 2;
+    else
+        cout << 3;
+}
