4fcb0d6d1c0467ffee974267c28a91eb990a5bfe
kkeagle3 <141285527+kkeagle3@users.noreply.github.com>
2026-01-16 20:58:23 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.cc
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -1,10 +1,12 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
 
 int main()
 {
-    int r1,r2,r3,r4,r5;
-    cin >> r1 >> r2 >> r3 >> r4 >> r5;
-    cout << (r1*r1+r2*r2+r3*r3+r4*r4+r5*r5)%10;
+    int a,n=0;
+    for(int i=0;i<5;i++){
+        cin >> a;
+        n+=a*a;
+    }
+    cout << n%10;
 }
