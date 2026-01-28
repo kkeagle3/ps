74948f4d4d28dd00c2f386139b0466a6a16ecc2a
kkeagle3 <141285527+kkeagle3@users.noreply.github.com>
2026-01-28 15:50:25 +0900

    [Bronze V] Title: 이중 반복문, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34849. 이중 반복문/이중 반복문.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int n;
+    cin >> n;
+    if(n*n>100000000LL)  cout << "Time limit exceeded";
+    else    cout << "Accepted";
+}
