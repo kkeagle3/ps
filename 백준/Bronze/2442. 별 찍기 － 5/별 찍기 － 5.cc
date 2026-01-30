eedbb2d24040a297ebe1234e5c075f5bc3f8894e
kkeagle3 <141285527+kkeagle3@users.noreply.github.com>
2026-01-30 20:44:43 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -1,15 +1,13 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    int N;
-    cin >> N;
-    for(int i=1;i<N+1;i++){
-        for(int j=N-i;j>0;j--)
-            cout << " ";
-        for(int j=2*i-1;j>0;j--)
-            cout << "*";
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++)    cout << " ";
+        for(int j=0;j<2*i+1;j++)    cout << "*";
         cout << "\n";
     }
 }
