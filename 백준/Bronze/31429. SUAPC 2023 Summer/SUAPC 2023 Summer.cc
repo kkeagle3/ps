26097cd052bd1edaff517b4514a92448b9c51404
kkeagle3 <141285527+kkeagle3@users.noreply.github.com>
2026-02-04 12:27:25 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[11]={12,11,11,10,9,9,9,8,7,6,6};
+    int arr2[11]={1600,894,1327,1311,1004,1178,1357,837,1055,556,773};
+    int n;
+    cin >> n;
+    cout << arr[n-1] << " " << arr2[n-1];
+}
